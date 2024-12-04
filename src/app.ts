2a8338b8660f097337758f6b882e309c2a4da319
inflection-prashant <prashant.kharade@inflectionzone.com>
2024-12-04 16:10:15 +0530

    Remove unnecessary "tail" variable from DATABASE_URL parsing.

--- src/app.ts
+++ src/app.ts
@@ -98,7 +98,7 @@ export default class Application {
             throw new Error('DATABASE_URL format is incorrect');
         }
 
-        const [_, user, password, tail, host, port, database] = matches;
+        const [_, user, password, host, port, database] = matches;
 
         try {
             const connection = await mysql.createConnection({
